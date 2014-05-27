40932bbd15f4c88905eaf46fee1e3a95c2dcc09c
Rémy Greinhofer <remy.greinhofer@gmail.com>
2014-05-26 17:58:02 -0700

    Implementing the KbModels in the OSD plugin.

--- kbextractor/plugins/destinations/osd.py
+++ kbextractor/plugins/destinations/osd.py
@@ -33,5 +33,3 @@ class OnDiskPlugin(IDestinationPlugin):
 
         # Display the subject of the articles
         [print("   {0}".format(article_entry.subject)) for article_entry in article_entry_list]
-        #for article_entry in article_entry_list:
-            #print ("   {0}".format(article_entry.subject))
