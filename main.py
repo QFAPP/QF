785d17e9277c92e58a308fe33eb556e78a0ed33c
Rémy Greinhofer <remy.greinhofer@gmail.com>
2014-05-31 19:19:23 -0700

    Reflecting the changes to the main file.

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import argparse
 
 from kbextractor.kb_plugin_manager import KbExtractorPluginManager
+from kbextractor.extractor import KbExtractor
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Export you knowledge base.")
+    parser.add_argument("--source", dest="source", action="store",
+                        help="Where you want to extract the data from")
+    parser.add_argument("--destination", dest="destination", action="store",
+                        help="Where you want to store the data")
+    parser.add_argument("-sopt", "--source-options", dest="source_options", action="store",
+                        nargs="?", const="", default="",
+                        help="The options for the source plugin")
+    parser.add_argument("-dopt", "--destination-options", dest="destination_options", action="store",
+                        nargs="?", const="", default="",
+                        help="The options for the destination plugin")
+    """
     parser.add_argument("--subdomain", dest="subdomain", action="store",
                         help="Your subdomain")
     parser.add_argument("--username", dest="username", action="store",
                         help="Your username")
     parser.add_argument("--password", dest="password", action="store",
                         help="Your password")
+    """
     parser.add_argument("--list", action="store_true",
                         help="list available plugins")
 
@@ -31,58 +44,13 @@ if __name__ == "__main__":
     # Set the source plugin
     sourcePluginInfo = kbManager.manager.getPluginByName("PyDesk", "Source")
     sourcePlugin = sourcePluginInfo.plugin_object
+    sourcePlugin.parse_options(args.source_options)
 
     # Set the destination plugin
-    destinationPluginInfo = kbManager.manager.getPluginByName("OnDisk", "Destination")
+    destinationPluginInfo = kbManager.manager.getPluginByName("OSD", "Destination")
     destinationPlugin = destinationPluginInfo.plugin_object
+    destinationPlugin.options_dict = {}
 
-    # Authenticate
-    sourcePlugin.authenticate(args.subdomain, args.username, args.password)
-
-    # Retrieve the topics
-    retrieved_topics = sourcePlugin.retrieve_items("topics")
-
-    while True:
-        # We can leave if there are no more topics
-        if not retrieved_topics:
-            break
-
-        # Set the entries and the meta
-        topic_entries = retrieved_topics[0]
-        topic_meta = retrieved_topics[1]
-
-        # Go through the topic list
-        for topic_entry in topic_entries:
-            topic_entry_name = topic_entry.name
-
-            # Retrieve the articles associated to this entry
-            retrieved_articles = sourcePlugin.retrieve_articles(topic_entry)
-            while True:
-                # We can leave if there are no more articles
-                if not retrieved_articles:
-                    break
-
-                # Set the entries and the meta
-                articles_entries = retrieved_articles[0]
-                articles_meta = retrieved_articles[1]
-
-                # Store the results
-                destinationPlugin.store(topic_entry_name, articles_entries)
-
-                # Retrieve the next page
-                articles_next_page_url = sourcePlugin.next_page(articles_meta)
-
-                # If there is no next page, we're done for this topic
-                if not articles_next_page_url:
-                    break
-
-                # Retrieve the next page of articles
-                retrieved_articles = sourcePlugin.retrieve_items(articles_next_page_url)
-
-        # Check whether there are more topics
-        topics_next_page_url = sourcePlugin.next_page(topic_meta)
-        if not topics_next_page_url:
-            break
-
-        # Retrieve the next page of topics
-        retrieved_topics = sourcePlugin.retrieve_items(topics_next_page_url)
+    # Start the extraction
+    extractor = KbExtractor(sourcePlugin, destinationPlugin)
+    extractor.extract()
